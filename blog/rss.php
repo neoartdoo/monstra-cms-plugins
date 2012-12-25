46976c768c4d34dd9fc2ad9988d76efa4b6c965e
Awilum <awilum@msn.com>
2012-12-25 21:16:26 +0200

    Blog: update rss.php

--- blog/rss.php
+++ blog/rss.php
@@ -14,25 +14,27 @@ $posts = Pages::$pages->select('[parent="blog" and status="published"]', 5, 0, a
 $now = date("D, d M Y H:i:s T");
 ob_end_clean();
 ?>
+<?php header('Content-type: text/xml; charset="utf-8"'); ?>
 <?php echo'<?xml version="1.0" encoding="utf-8"?>'."\n"; ?>
-<?php header('Content-type: text/xml'); ?>
-<feed xml:lang="en-US" xmlns="http://www.w3.org/2005/Atom"> 
+<rss version="2.0" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:wfw="http://wellformedweb.org/CommentAPI/" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:atom="http://www.w3.org/2005/Atom">
+<channel>
 <title>MonstraCMS::BLOG::RSS</title>
-<link><?php echo Option::get('siteurl'); ?></link>
-<description>MonstraCMS::BLOG::RSS</description>
+<link><?php echo Option::get('siteurl'); ?>blog</link>
+<description>The latest updates for <?php echo Option::get('sitename'); ?>.</description>
 <language>en-us</language>
 <pubDate><?php echo $now; ?></pubDate>
 <lastBuildDate><?php echo $now; ?></lastBuildDate>
+<atom:link href="<?php echo Option::get('siteurl'); ?>rss.php" rel="self" type="application/rss+xml" />
 <generator>Monstra</generator>
-<?php    
-foreach ($posts as $post) {
-echo'<entry>
-<title>'.$post['title'].'</title>          
-<link type=\'text/html\' href=\''.Option::get('siteurl').'blog/'.$post['slug'].'\' />
-<summary>'.Text::cut(File::getContent(STORAGE . DS . 'pages' . DS . $post['id'] . '.page.txt'), 100).'</summary>
-<author>'.$post['author'].'</author>
-<pubDate>'.Date::format($post['date'], 'd M Y').'</pubDate>
-</entry>';
-}
-?>    
-</feed>
+<?php foreach ($posts as $post) { ?>
+<item>
+<title><?php echo $post['title']; ?></title>
+<link><?php echo Option::get('siteurl').'blog/'.$post['slug']; ?></link>
+<guid><?php echo Option::get('siteurl').'blog/'.$post['slug']; ?></guid>
+<pubDate><?php echo Date::format($post['date'], 'd M Y'); ?></pubDate>
+<description><![CDATA[<?php echo Text::toHtml(Text::cut(File::getContent(STORAGE . DS . 'pages' . DS . $post['id'] . '.page.txt'), 300)); ?>]]></description>
+<author><?php echo $post['author']; ?></author>
+</item>
+<?php } ?>
+</channel>
+</rss>
