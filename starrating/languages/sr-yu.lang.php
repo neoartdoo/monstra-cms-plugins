158f377ee0bd413223109f5a7b13dd8a74341b97
mapadesign <panic.marjan@gmail.com>
2013-01-13 07:06:07 -0800

    Create sr-yu.lang.php

---
+++ starrating/languages/sr-yu.lang.php
@@ -0,0 +1,12 @@
+//serbian language for monstra
+<?php
+
+    return array(
+        'starrating' => array(
+            'Star Rating' => 'Rejting',
+            'Star Rating plugin for Monstra' => 'Rejting dodatak za Monstra',
+            'Vote' => 'Glas',
+            'Votes' => 'Glasova',
+            'Rating' => 'Rejting',
+        ) 
+    );
