611622b25113ecb76430a6ac6bec80848762c9c8
mapadesign <panic.marjan@gmail.com>
2013-01-13 07:04:23 -0800

    Create sr-yu.lang.php

---
+++ guestbook/languages/sr-yu.lang.php
@@ -0,0 +1,22 @@
+//Serbian Language
+<?php
+
+    return array(
+        'guestbook' => array(
+            'Guestbook' => 'Knjiga Gostiju',
+            'Guest book plugin for Monstra' => 'Knjiga Gostiju dodatak za Monstra',
+            'Add comment' => 'Dodaj komentar',
+            'Empty required fields!' => 'Prazna polja su potrebna!',
+            'Email address is not valid!' => 'Email Adresa je potrebna!',
+            'Captcha answer is wrong!' => 'Odgovor antispam botu nije ispravan!',
+            'Username' => 'Korisničko ime',
+            'Email' => 'Email',
+            'Message' => 'Poruka',
+            'Send' => 'Pošalji',
+            'Guestbook Template' => 'Knjiga gostiju Šablon',
+            'Actions' => 'Akcije',
+            'Date' => 'datum',
+            'Delete' => 'Obriši',
+            'Delete record' => 'Obriši zapis',
+        )
+    );
