d083a0e0ab56e49b79334f15b21e577ed8cbdbb8
mapadesign <panic.marjan@gmail.com>
2013-01-13 07:01:40 -0800

    Create sr-yu.lang.php

---
+++ contact/languages/sr-yu.lang.php
@@ -0,0 +1,18 @@
+// Serbian Language 
+<?php
+
+    return array(
+        'contact' => array(
+            'Contact' => 'Kontakt',
+            'Contact plugin for Monstra' => 'Kontakt dodatak za Monstra',
+            'Name' => 'Ime',
+            'Email' => 'Email',
+            'Message' => 'Poruka',
+            'Send' => 'Pošalji',
+            'Empty required fields!' => 'Prazna polja su potrebna!',
+            'Email address is not valid!' => 'Email adresa nije validna!',
+            'A letter has been sent!' => 'Vaša pošta je poslata!',
+            'A Letter was not sent!' => 'Vaša pošta nije poslata!',
+            'Wrong captcha!' => 'Pogrešan kod sa slike!',
+        ),
+    );
